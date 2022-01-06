d16c2aa5c2e9aa4ed6c913b135e5e61f04dde3d9
Juan Scrocchi <scrocchi@gmail.com>
2022-01-06 10:18:26 -0400

    Change page title/description

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "Tea Shack®",
+    description: "Have a cup of positivitea.",
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
